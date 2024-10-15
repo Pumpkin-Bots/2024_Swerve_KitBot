f9726fb6d3bbcab49a0841320ed0c3244f4d83d6
Alex <emrieaj@gmail.com>
2024-10-14 18:03:06 -0700

    Remove unused imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,13 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.SmartDashboardConstants;
-import frc.robot.Constants.OperatorConstants.ControllerConfigMode;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
